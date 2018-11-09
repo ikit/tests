1efab886b08df131924dc40df232b6d6d3d00044
ikit <olivier@labsquare.org>
2018-11-09 15:49:14 +0100

    tp6

---
+++ formations/ReacJs by Kumquats/5.1-redux/demarrage/src/js/store/configureStore.js
@@ -0,0 +1,12 @@
+import { createStore, compose, applyMiddleware } from "redux";
+import reducer from "../reducers";
+import thunk from "redux-thunk";
+
+export default function() {
+    const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+    const store = createStore(reducer, composeEnhancers(
+        applyMiddleware(thunk)
+    ));
+
+    return store;
+}
